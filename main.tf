685cf14702deaf0cd08c8d0837a4995472d99799
syedjohn1786 <syedjohn1786@gmail.com>
2023-12-09 02:40:05 +0530

    successfully hosted a website in S3 Bucket

--- main.tf
+++ main.tf
@@ -3,3 +3,60 @@
 resource "aws_s3_bucket" "bucketone" {
   bucket = var.nameofthebucket
 }
+
+resource "aws_s3_bucket_ownership_controls" "bucketone" {
+  bucket = aws_s3_bucket.bucketone.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucketone" {
+  bucket = aws_s3_bucket.bucketone.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "bucketone" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucketone,
+    aws_s3_bucket_public_access_block.bucketone,
+  ]
+
+  bucket = aws_s3_bucket.bucketone.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket = aws_s3_bucket.bucketone.id
+  key    = "index.html"
+  source = "index.html"
+  acl = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "error" {
+  bucket = aws_s3_bucket.bucketone.id
+  key    = "error.html"
+  source = "error.html"
+  acl = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.bucketone.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  depends_on = [ aws_s3_bucket_acl.bucketone ]
+}
